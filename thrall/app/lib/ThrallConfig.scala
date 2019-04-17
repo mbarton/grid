9ab5cfd7563c9a4996f6f4962c2fb8eb76eef3cc
Akash Askoolum <akash1810@gmail.com>
2019-04-17 17:33:39 +0100

    es1 and es6 config is always present
    update thrall, removing the Option types

--- thrall/app/lib/ThrallConfig.scala
+++ thrall/app/lib/ThrallConfig.scala
@@ -20,7 +20,7 @@ class ThrallConfig(override val configuration: Configuration) extends CommonConf
 
   lazy val thumbnailBucket: String = properties("s3.thumb.bucket")
 
-  lazy val elasticsearchHost: Option[String] = Some {
+  lazy val elasticsearchHost: String =
     if (isDev)
       properties.getOrElse("es.host", "localhost")
     else
@@ -29,22 +29,15 @@ class ThrallConfig(override val configuration: Configuration) extends CommonConf
         "Stack" -> Seq(elasticsearchStack),
         "App" -> Seq(elasticsearchApp)
       ))
-  }
 
-  lazy val elasticsearchPort: Option[Int] = properties.get("es.port").map(_.toInt)
-  lazy val elasticsearchCluster: Option[String] = properties.get("es.cluster")
+  lazy val elasticsearchPort: Int = properties("es.port").toInt
+  lazy val elasticsearchCluster: String = properties("es.cluster")
 
-  lazy val elasticsearch6Host: Option[String] =  {
-    if (isDev)
-      Some(properties.getOrElse("es6.host", "localhost"))
-    else
-      properties.get("es6.host")
-  }
-
-  lazy val elasticsearch6Port: Option[Int] = properties.get("es6.port").map(_.toInt)
-  lazy val elasticsearch6Cluster: Option[String] = properties.get("es6.cluster")
-  lazy val elasticsearch6Shards = Some(if (isDev) 1 else properties("es6.shards").toInt)
-  lazy val elasticsearch6Replicas = Some(if (isDev) 0 else properties("es6.replicas").toInt)
+  lazy val elasticsearch6Host: String =  properties("es6.host")
+  lazy val elasticsearch6Port: Int = properties("es6.port").toInt
+  lazy val elasticsearch6Cluster: String = properties("es6.cluster")
+  lazy val elasticsearch6Shards: Int = properties("es6.shards").toInt
+  lazy val elasticsearch6Replicas: Int = properties("es6.replicas").toInt
 
   lazy val healthyMessageRate: Int = properties("sqs.message.min.frequency").toInt
 

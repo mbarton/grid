c032dd9dca3edaa0986f99d4c456b08427f86fec
Akash Askoolum <akash1810@gmail.com>
2019-04-17 17:31:59 +0100

    es1 and es6 config is always present
    update media-api, removing the Option types

--- media-api/app/lib/MediaApiConfig.scala
+++ media-api/app/lib/MediaApiConfig.scala
@@ -42,19 +42,14 @@ class MediaApiConfig(override val configuration: Configuration) extends CommonCo
         "App"   -> Seq(elasticsearchApp)
       ))
 
-  lazy val elasticsearchPort: Option[Int] = properties.get("es.port").map(_.toInt)
-  lazy val elasticsearchCluster: Option[String] = properties.get("es.cluster")
+  lazy val elasticsearchPort: Int = properties("es.port").toInt
+  lazy val elasticsearchCluster: String = properties("es.cluster")
 
-  lazy val elasticsearch6Host: Option[String] =  {
-    if (isDev)
-      Some(properties.getOrElse("es6.host", "localhost"))
-    else
-      properties.get("es6.host")
-  }
-  lazy val elasticsearch6Port: Option[Int] = properties.get("es6.port").map(_.toInt)
-  lazy val elasticsearch6Cluster: Option[String] = properties.get("es6.cluster")
-  lazy val elasticsearch6Shards = Some(if (isDev) 1 else properties("es6.shards").toInt)
-  lazy val elasticsearch6Replicas = Some(if (isDev) 0 else properties("es6.replicas").toInt)
+  lazy val elasticsearch6Host: String =  properties("es6.host")
+  lazy val elasticsearch6Port: Int = properties("es6.port").toInt
+  lazy val elasticsearch6Cluster: String = properties("es6.cluster")
+  lazy val elasticsearch6Shards: Int = properties("es6.shards").toInt
+  lazy val elasticsearch6Replicas: Int = properties("es6.replicas").toInt
 
   lazy val imageBucket: String = properties("s3.image.bucket")
   lazy val thumbBucket: String = properties("s3.thumb.bucket")
